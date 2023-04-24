d503b765ee7079ddfc2357c8572ce59242d8c810
guxttavo <gustavowbarbosas@gmail.com>
2023-04-24 14:11:00 -0300

    AnimalConfiguration

---
+++ TobyVet.Data/Configuration/Application/AnimalConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TobyVet.Core.Models;
+
+namespace TobyVet.Data.Configuration.Application
+{
+    public class AnimalConfiguration : IEntityTypeConfiguration<Animal>
+    {
+        public void Configure(EntityTypeBuilder<Animal> builder)
+        {
+            builder.ToTable("animal", "dbo");
+
+            builder.HasKey(x => x.Id).HasName("pk_Animal");
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().HasColumnName("id");
+            builder.Property(x => x.Nome).HasColumnName("nome");
+            builder.Property(x => x.Responsavel).HasColumnName("responsavel");
+            builder.Property(x => x.Especie).HasColumnName("especie");
+            builder.Property(x => x.Genero).HasColumnName("genero");
+            builder.Property(x => x.Peso).HasColumnName("peso");
+            builder.Property(x => x.Altura).HasColumnName("altura");
+            builder.Property(x => x.Cor).HasColumnName("cor");
+        }
+    }
+}
--- TobyVet.Web/Configurations/DependecyInjection.cs
+++ TobyVet.Web/Configurations/DependecyInjection.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using TobyVet.Core.Settings;
 using TobyVet.Data;
 
