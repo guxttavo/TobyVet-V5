ec1bb0525ab08c13c3e19279d659c170f83d1618
guxttavo <gustavowbarbosas@gmail.com>
2023-04-24 14:38:01 -0300

    Interface Repository e Service

---
+++ TobyVet.Core/Interfaces/Repositories/IAnimalRepository.cs
@@ -0,0 +1,10 @@
+using TobyVet.Core.Models;
+
+namespace TobyVet.Core.Interfaces.Repositories
+{
+    public interface IAnimalRepository
+    {
+        Task<IEnumerable<Animal>> BuscarAnimais();
+        
+    }
+}
---
+++ TobyVet.Core/Interfaces/Services/IAnimalService.cs
@@ -0,0 +1,9 @@
+using TobyVet.Core.Models;
+
+namespace TobyVet.Core.Interfaces.Services
+{
+    public interface IAnimalService
+    {
+         Task<IEnumerable<Animal>> BuscarAnimaisService();
+    }
+}
