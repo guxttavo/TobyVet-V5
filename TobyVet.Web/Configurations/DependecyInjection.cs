6ed682047fb357a385c75ef3de402708d5472cd3
guxttavo <gustavowbarbosas@gmail.com>
2023-04-24 13:57:17 -0300

    disable on nullable

---
+++ TobyVet.Web/Configurations/DependecyInjection.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using TobyVet.Core.Settings;
+using TobyVet.Data;
+
+namespace TobyVet.Web.Configurations
+{
+    public static class DependecyInjection
+    {
+        public static void AddDependencies(this IServiceCollection services, AppSettings appSettings)
+        {
+            services.AddSingleton(appSettings);
+
+            services.AddControllersWithViews();
+
+            services.AddScoped<ApplicationDbContext>();
+
+        }
+
+    }
+}
